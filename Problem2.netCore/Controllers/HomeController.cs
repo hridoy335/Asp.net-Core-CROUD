faee9bbb92c488e10665a3a46b7ccd577a43c218
Hridoy Roy <15103182@iubat.edu>
2020-12-20 01:26:33 +0600

    Data successfully view in index page 12/20/2020

--- Problem2.netCore/Controllers/HomeController.cs
+++ Problem2.netCore/Controllers/HomeController.cs
@@ -6,25 +6,67 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Problem2.netCore.Models;
+using System.Data.SqlClient;
 
 namespace Problem2.netCore.Controllers
 {
     public class HomeController : Controller
     {
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        List<Employee> emp = new List<Employee>(); 
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            con.ConnectionString = Problem2.netCore.Properties.Resources.ConnectionString;
+
         }
 
         public IActionResult Index()
         {
-            return View();
+            Getdata();
+            return View(emp);
         }
 
+        private void Getdata()
+        {
+            if (emp.Count > 0)
+            {
+                emp.Clear();
+            }
+
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT [EmpId],[EmployeeName],[EmployeeAddress],[EmployeeContact],[EmployeeEmail] FROM [TestEmployeeDB].[dbo].[EmployeeTbl]";
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    emp.Add(new Employee() {
+                        EmpId=Convert.ToInt32(dr["EmpId"].ToString()),
+                        EmployeeName=dr["EmployeeName"].ToString(),
+                        EmployeeAddress=dr["EmployeeAddress"].ToString(),
+                        EmployeeContact=dr["EmployeeContact"].ToString(),
+                        EmployeeEmail=dr["EmployeeEmail"].ToString(),
+                        
+                        });
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
         public IActionResult Privacy()
         {
+
             return View();
         }
 
---
+++ Problem2.netCore/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Problem2.netCore.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeAddress { get; set; }
+        public string EmployeeContact { get; set; }
+        public string EmployeeEmail { get; set; }
+    }
+}
