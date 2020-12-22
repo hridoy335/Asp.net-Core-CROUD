3a719ceaff9fe2705336f06a4f7a3befa24248c9
Hridoy Roy <15103182@iubat.edu>
2020-12-22 21:45:00 +0600

    Add service for update employee information

---
+++ Problem2.netCore/Service/EmployeeDML.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Problem2.netCore.Service
+{
+    public class EmployeeDML
+    {
+    }
+}
